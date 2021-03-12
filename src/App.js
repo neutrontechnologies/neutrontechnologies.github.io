6a07758682d78bd47f7111d24d6cf0c75824039b
Oxygemo <alej0hio2007@gmail.com>
2021-03-12 15:34:23 +0100

    add discord button and make buttongroup from styling components
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const ButtonGroup = styled.div`
+  display: flex;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Neutron Technologies</p>
-        <a
-          className="Neutron-Tek"
-          href="https://github.com/neutrontechnologies"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Check out our organization!"
-        >
-          <button>Organization page</button>
-        </a>
-        
-        
+        <p>Neutron Technologies</p>
+        <ButtonGroup>
+          <a
+            className="Neutron-Tek"
+            href="https://github.com/neutrontechnologies"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Check out our organization!"
+          >
+            <button>Organization page</button>
+          </a>
+          &nbsp;
+          <a
+            className="Neutron-Tek"
+            href="https://discord.gg/HYrafUqq73"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Join our Discord server!">
+            <button>Discord</button>
+          </a>
+        </ButtonGroup>
       </header>
     </div>
   );
